32fe6b074e67677ad5c55fbf7f97ab5b7d87514f
Victoria-Vladimirova <victoria@MacBook-Air-Victoria.local>
2015-11-22 22:35:40 +0500

    add solution

--- src/standard-library.js
+++ src/standard-library.js
@@ -1,27 +1,237 @@
 'use strict';
 
 var Collection = function () {
+    this._init();
+};
 
+Collection.prototype._init = function () {
+    this.length = 0;
+    this.first = null;
+    this.last = null;
+    this.isEmpty = true;
+    this.collection = [];
+};
+
+Collection.prototype._refresh = function () {
+    this.first = this.collection[0];
+    this.last = this.collection[this.collection.length - 1];
+    this.length = this.collection.length;
+    this.isEmpty = this.length === 0;
+};
+
+// извлекает первый элемент из коллекции и возвращает его
+Collection.prototype.pickFirst = function () {
+    if (this.isEmpty) {
+        return null;
+    }
+    var first = this.collection.shift();
+    this._refresh();
+    return first;
+};
+
+// извлекает последний элемент из коллекции и возвращает его
+Collection.prototype.pickLast = function () {
+    if (this.isEmpty) {
+        return null;
+    }
+    var last = this.collection.pop();
+    this._refresh();
+    return last;
+};
+
+// вставляет элемент в начало колекции
+Collection.prototype.insertFirst = function (item) {
+    this.collection.unshift(item);
+    this._refresh();
+};
+
+// вставляет элемент в конец коллекции
+Collection.prototype.insertLast = function (item) {
+    this.collection.push(item);
+    this._refresh();
+};
+
+// очищает коллекцию
+Collection.prototype.empty = function () {
+    this._init();
 };
 
 var Queue = function () {
+    Collection.apply(this, arguments);
+};
 
+Queue.prototype = Object.create(Collection.prototype);
+Queue.prototype.constructor = Queue;
+
+// добавляет элемент в очередь
+Queue.prototype.enqueue = function (item) {
+    this.insertLast(item);
+};
+
+// извлекает элемент из очереди
+Queue.prototype.dequeue = function () {
+    return this.pickFirst();
 };
 
 var FixedArray = function (size) {
+    if (size <= 0 || (isNaN(parseFloat(size)) && !isFinite(size))) {
+        throw new RangeError('RangeError');
+    }
+    this.length = size;
+    this.collection = [];
+};
 
+// записывает элемент в массив по заданному индексу
+FixedArray.prototype.insertAt = function (index, item) {
+    if (index < 0 || index >= this.length) {
+        throw new RangeError('Parameter must be between ' + 0 + ' and ' + this.length);
+    }
+    if (item === undefined) {
+        throw new RangeError('Missing expected exception');
+    }
+    this.collection[index] = item;
+};
+
+// возвращает элемент по указанному индексу
+FixedArray.prototype.getAt = function (index) {
+    if (index < 0 || index >= this.length) {
+        throw new RangeError('Parameter must be between ' + 0 + ' and ' + this.length);
+    }
+    return this.collection[index] !== undefined ? this.collection[index] : null;
 };
 
 var Set = function () {
+    Collection.apply(this, arguments);
+};
 
+Set.prototype = Object.create(Collection.prototype);
+Set.prototype.constructor = Set;
+
+// добавляет элемент в множество
+Set.prototype.insert = function (item) {
+    if (!this.has(item)) {
+        this.insertLast(item);
+    }
+};
+
+// удаляет элемент из множества
+Set.prototype.remove = function (item) {
+    if (this.has(item)) {
+        this.collection.splice(this.collection.indexOf(item), 1);
+        this._refresh();
+    }
+};
+
+// проверяет, входит ли элемент в множество
+Set.prototype.has = function (item) {
+    return this.collection.indexOf(item) !== -1;
+};
+
+// возвращает множество элементов входящих в исходное множество и в переданное множество
+Set.prototype.intersect = function (set) {
+    var res = new Set();
+    this.collection.forEach(function (item) {
+        if (set.has(item)) {
+            res.insert(item);
+        }
+    });
+    return res;
+};
+
+// возвращает множество элементов входящих в исходное множество или в переданное множество
+Set.prototype.union = function (set) {
+    var res = new Set();
+    this.collection.forEach(function (item) {
+        res.insert(item);
+    });
+    set.collection.forEach(function (item) {
+        res.insert(item);
+    });
+    return res;
 };
 
 var PriorityQueue = function () {
+    Collection.apply(this, arguments);
+};
 
+PriorityQueue.prototype = Object.create(Queue.prototype);
+PriorityQueue.prototype.constructor = PriorityQueue;
+
+// кладёт элемент с priority (целое число в интервале от 1 до 100)
+PriorityQueue.prototype.enqueue = function (item, priority) {
+    if (priority < 1 || priority > 100) {
+        throw new RangeError('Parameter must be between 1 and 100');
+    }
+    priority = priority || 1;
+    Queue.prototype.enqueue.call(this, {item: item, priority: priority});
+};
+
+// извлекает элемент с наивысшим приоритетом из очереди
+PriorityQueue.prototype.dequeue = function () {
+    if (this.isEmpty) {
+        return null;
+    }
+    var maxPriority = 0;
+    var res;
+    var resIndex;
+    for (var i = 0; i < this.length; i++) {
+        if (this.collection[i].priority > maxPriority) {
+            maxPriority = this.collection[i].priority;
+            res = this.collection[i].item;
+            resIndex = i;
+        }
+    }
+    this.collection.splice(resIndex, 1);
+    this._refresh();
+    return res;
 };
 
 var Map = function () {
+    this._init();
+};
 
+Map.prototype._init = function () {
+    this.length = 0;
+    this.isEmpty = true;
+    this.elements = {};
+};
+
+Map.prototype._refresh = function () {
+    this.length = Object.keys(this.elements).length;
+    this.isEmpty = this.length === 0;
+};
+
+Map.prototype._keyToString = function (key) {
+    return typeof key === 'function' ? key.toString() : JSON.stringify(key);
+};
+
+// добавляет элемент в словарь по указанному ключу
+Map.prototype.addItem = function (key, item) {
+    if (this.getItem(key) === null) {
+        this.elements[this._keyToString(key)] = item;
+    }
+    this._refresh();
+};
+
+// извлекает элемент из словаря по указанному ключу
+Map.prototype.removeItem = function (key) {
+    var item;
+    if (item = this.getItem(key)) {
+        delete this.elements[this._keyToString(key)];
+        this._refresh();
+        return item;
+    }
+};
+
+// возвращает элемент по указанному ключу
+Map.prototype.getItem = function (key) {
+    key = this._keyToString(key);
+    return this.elements[key] ? this.elements[key] : null;
+};
+
+// очищает словарь
+Map.prototype.empty = function () {
+    this._init();
 };
 
 exports.Collection = Collection;
--- test/public.js
+++ test/public.js
@@ -8,6 +8,7 @@ var FixedArray = lib.FixedArray;
 var Queue = lib.Queue;
 var Set = lib.Set;
 var PriorityQueue = lib.PriorityQueue;
+var Map = lib.Map;
 
 describe('Standard Library', function () {
     describe('Collection', function () {
@@ -232,6 +233,103 @@ describe('Standard Library', function () {
             assert.ok(set3.has('foo'));
             assert.ok(set3.has('bar'));
             assert.ok(set3.has('baz'));
+        });
+    });
+
+    // доп. задание
+
+    describe('PriorityQueue', function () {
+        it('enqueue(item, priority)', function () {
+            var queue = new PriorityQueue();
+
+            assert.equal(queue.length, 0);
+
+            queue.enqueue('foo', 2);
+            queue.enqueue('bar');
+            queue.enqueue('baz', 1);
+
+            assert.equal(queue.length, 3);
+        });
+
+        it('dequeue()', function () {
+            var queue = new PriorityQueue();
+            queue.enqueue('foo', 1);
+            queue.enqueue('bar', 3);
+            queue.enqueue('baz', 2);
+
+            assert.equal(queue.dequeue(), 'bar');
+            assert.equal(queue.dequeue(), 'baz');
+            assert.equal(queue.dequeue(), 'foo');
+        });
+    });
+
+    describe('Map', function () {
+        it('addItem(key, item)', function () {
+            var map = new Map();
+
+            assert.equal(map.length, 0);
+
+            map.addItem({}, 2);
+            map.addItem([], 2);
+            map.addItem(new Date(), 2);
+
+            assert.equal(map.length, 3);
+        });
+
+        it('removeItem(key)', function () {
+            var map = new Map();
+
+            assert.equal(map.length, 0);
+
+            map.addItem({}, 2);
+            map.addItem([], 'aaa');
+            map.addItem(new Date(), 6);
+
+            map.removeItem({});
+            map.removeItem(new Date());
+
+            assert.equal(map.length, 1);
+        });
+
+        it('getItem(key)', function () {
+            var map = new Map();
+
+            assert.equal(map.length, 0);
+
+            var date = new Date();
+
+            map.addItem({}, 2);
+            map.addItem([], 'aaa');
+            map.addItem(date, 6);
+
+            assert.equal(map.getItem(date), 6);
+        });
+        it('getItem(function)', function () {
+            var map = new Map();
+
+            assert.equal(map.length, 0);
+
+            var f1 = function () {
+                console.log('1');
+            };
+            var f2 = function () {
+                console.log('2');
+            };
+            map.addItem(f1, 1);
+            map.addItem(f2, 2);
+            assert.equal(map.length, 2);
+        });
+        it('empty()', function () {
+            var map = new Map();
+
+            assert.equal(map.length, 0);
+
+            map.addItem({}, 2);
+            map.addItem([], 'aaa');
+            map.addItem(new Date(), 6);
+            map.empty();
+
+            assert.ok(map.isEmpty);
         });
     });
 });
