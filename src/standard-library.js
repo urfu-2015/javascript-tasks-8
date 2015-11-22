8c7717a48a58faa775bd75198f2375d52435f0c3
Nikita <nikshelekb@yandex.ru>
2015-11-22 19:50:37 +0500

    Made task

--- src/standard-library.js
+++ src/standard-library.js
@@ -1,20 +1,122 @@
 'use strict';
 
-var Collection = function () {
+class Collection {
+    constructor() {
+        this.array = [];
+        Object.defineProperty(this, 'first', { get: function () {
+            return this.array[0];
+        } });
+        Object.defineProperty(this, 'last', { get: function () {
+            return this.array[this.array.length - 1];
+        } });
+        Object.defineProperty(this, 'length', { get: function () {
+            return this.array.length;
+        }, configurable: true});
+        Object.defineProperty(this, 'isEmpty', { get: function () {
+            return this.array.length === 0;
+        } });
+    }
 
-};
+    pickFirst() {
+        return this.array.shift();
+    }
 
-var Queue = function () {
+    pickLast() {
+        return this.array.pop();
+    }
 
-};
+    insertFirst(item) {
+        this.array.unshift(item);
+    }
 
-var FixedArray = function (size) {
+    insertLast(item) {
+        this.array.push(item);
+    }
 
-};
+    empty() {
+        this.array = [];
+    }
+}
 
-var Set = function () {
+class Queue extends Collection {
+    constructor() {
+        super();
 
-};
+        this.enqueue = this.insertLast;
+        this.dequeue = this.pickFirst;
+    }
+}
+
+class FixedArray extends Collection {
+    constructor(maxLength) {
+        super();
+        Object.defineProperty(this, 'length', { get: function () {
+            return maxLength;
+        } });
+    }
+
+    isIndexCorrect(index) {
+        return index >= 0 && index < this.length;
+    }
+
+    insertAt(index, item) {
+        if (!this.isIndexCorrect(index)) {
+            throw RangeError();
+        }
+        this.array[index] = item;
+    }
+
+    getAt(index) {
+        if (!this.isIndexCorrect(index)) {
+            throw RangeError();
+        }
+        return this.array[index];
+    }
+}
+
+class Set extends Collection {
+    constructor() {
+        super();
+    }
+
+    has(item) {
+        return (this.array.indexOf(item) >= 0);
+    }
+
+    insert(item) {
+        if (!this.has(item)) {
+            this.insertLast(item);
+        }
+    }
+
+    remove(item) {
+        if (this.has(item)) {
+            this.array.splice(this.array.indexOf(item), 1);
+        }
+    }
+
+    intersect(otherSet) {
+        var newSet = new Set();
+        this.array.forEach(function (item) {
+            if (otherSet.has(item)) {
+                newSet.insert(item);
+            }
+        });
+        return newSet;
+    }
+
+    union(otherSet) {
+        var newSet = new Set();
+        this.array.forEach(function (item) {
+            newSet.insert(item);
+        });
+
+        otherSet.array.forEach(function (item) {
+            newSet.insert(item);
+        });
+        return newSet;
+    }
+}
 
 var PriorityQueue = function () {
 
