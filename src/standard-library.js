b47e8e958c8c3dd6213407a0b5d6ad2e5e335554
Dolgih Natalia <n.e.dolgih@yandex.ru>
2015-11-24 00:45:32 +0500

    fixes

--- src/standard-library.js
+++ src/standard-library.js
@@ -14,115 +14,122 @@ BasicCollection.prototype.empty = function () {
 var Collection = function () {
     this.first = null;
     this.last = null;
-    Collection.prototype.pickFirst = function () {
-        this.length--;
-        this.first = this.container[1];
-        return this.container.splice(0, 1)[0];
-    };
-    Collection.prototype.pickLast = function () {
-        this.length--;
-        this.last = this.container[-2];
-        return this.container.splice(-1, 1)[0];
-    };
-    Collection.prototype.insertFirst = function (obj) {
-        this.length++;
-        this.container.splice(0, 0, obj);
-        this.first = this.container[0];
-    };
-    Collection.prototype.insertLast = function (obj) {
-        this.length++;
-        this.container.splice(0, 0, obj);
-        this.last = this.container[-1];
-    }
-    Collection.prototype.isEmpty = function () {
-        return this.container.length === 0;
-    }
+    this.container = [];
+    this.length = 0;
 };
-Collection.prototype = Object.create(BasicCollection);
+Collection.prototype.pickFirst = function () {
+    this.length--;
+    this.first = this.container[1];
+    return this.container.splice(0, 1)[0];
+};
+Collection.prototype.pickLast = function () {
+    this.length--;
+    this.last = this.container[-2];
+    return this.container.splice(-1, 1)[0];
+};
+Collection.prototype.insertFirst = function (obj) {
+    this.length++;
+    this.container.splice(0, 0, obj);
+    this.first = this.container[0];
+};
+Collection.prototype.insertLast = function (obj) {
+    this.length++;
+    this.container.splice(0, 0, obj);
+    this.last = this.container[-1];
+};
+Collection.prototype.isEmpty = function () {
+    return this.container.length === 0;
+};
+
+Collection.prototype.empty = function () {
+    this.container = [];
+    this.length = 0;
+};
 
 var Queue = function () {
-    Queue.prototype.enqueue = function (item) {
-        this.container.push(item);
-        this.length++;
-    };
-    Queue.prototype.dequeue = function () {
-        this.length--;
-        return this.container.pop();
-    };
+    this.container = [];
+    this.length = 0;
 };
-Queue.prototype = Object.create(BasicCollection);
+Queue.prototype.enqueue = function (item) {
+    this.container.push(item);
+    this.length++;
+};
+Queue.prototype.dequeue = function () {
+    this.length--;
+    return this.container.pop();
+};
+Queue.prototype.empty = function () {
+    this.container = [];
+    this.length = 0;
+};
 
 var FixedArray = function (size) {
+    this.container = [];
+    this.length = 0;
     this.size = size;
-    //FixedArray.prototype.length Ц количество элементов в массиве
-    FixedArray.prototype.insertAt = function (index, item) {
-        if (index > this.size || index < 0) {
-            throw new RangeError('Index ' + index + ' out of range');
-        }
-        this.container[index] = item;
-    };// Ц записывает элемент в массив по заданному индексу
-    FixedArray.prototype.getAt = function (index) {
-        if (index > this.size || index < 0) {
-            throw new RangeError('Index ' + index + ' out of range');
-        }
-        return this.container[index]
-    };// Ц возвращает элемент по указанному индексу.
 };
 
-FixedArray.prototype = Object.create(BasicCollection);
+FixedArray.prototype.insertAt = function (index, item) {
+    if (index > this.size || index < 0) {
+        throw new RangeError('Index ' + index + ' out of range');
+    }
+    this.container[index] = item;
+};
+FixedArray.prototype.getAt = function (index) {
+    if (index > this.size || index < 0) {
+        throw new RangeError('Index ' + index + ' out of range');
+    }
+    return this.container[index]
+};
+
 
 var Set = function () {
-    Set.prototype.insert = function (item) {
-        if (this.container.indexOf(item) === -1) {
-            this.container.push(item);
-            this.length++;
-        }
-    }; // - добавл€ет элемент в множество
-    Set.prototype.remove = function (item) {
-        if (this.container.indexOf(item) !== -1) {
-            this.container.splice(this.container.indexOf(item), 1);
-            this.length--;
-        }
-    };// - удал€ет элемент из множества
-    Set.prototype.has = function (item) {
-        return this.container.indexOf(item) !== -1;
-    };// - провер€ет, входит ли элемент в множество
-    Set.prototype.intersect = function (set) {
-        var result = new Set();
-        for (var i = 0; i < set.length; i++) {
-            if (!this.has(set.container[i])) {
-                result.insert(set.container[i]);
-            }
-        }
-        return result;
-    };//Ц возвращает множество элементов вход€щих в исходное множество и в переданное множество (в оба сразу)
-    Set.prototype.union = function (set) {
-        var result = new Set();
-        for (var i = 0; i < set.length; i++) {
+    this.container = [];
+    this.length = 0;
+};
+Set.prototype.insert = function (item) {
+    if (this.container.indexOf(item) === -1) {
+        this.container.push(item);
+        this.length++;
+    }
+};
+Set.prototype.remove = function (item) {
+    if (this.container.indexOf(item) !== -1) {
+        this.container.splice(this.container.indexOf(item), 1);
+        this.length--;
+    }
+};
+Set.prototype.has = function (item) {
+    return this.container.indexOf(item) !== -1;
+};
+Set.prototype.intersect = function (set) {
+    var result = new Set();
+    for (var i = 0; i < set.length; i++) {
+        if (!this.has(set.container[i])) {
             result.insert(set.container[i]);
         }
-        for (i = 0; i < this.length; i++) {
-            if (!result.has(this.container[i])) {
-                result.insert(this.container[i]);
-            }
+    }
+    return result;
+};
+Set.prototype.union = function (set) {
+    var result = new Set();
+    for (var i = 0; i < set.length; i++) {
+        result.insert(set.container[i]);
+    }
+    for (i = 0; i < this.length; i++) {
+        if (!result.has(this.container[i])) {
+            result.insert(this.container[i]);
         }
-        return result;
-    };// Ц возвращает множество элементов вход€щих в исходное множество или в переданное множество (в любое из двух)
+    }
+    return result;
+};
+Set.prototype.empty = function () {
+    this.container = [];
+    this.length = 0;
 };
 
-Set.prototype = Object.create(BasicCollection);
-
-var PriorityQueue = function () {
-
-};
-
-var Map = function () {
-
-};
 
 exports.Collection = Collection;
 exports.Queue = Queue;
 exports.FixedArray = FixedArray;
 exports.Set = Set;
-exports.PriorityQueue = PriorityQueue;
-exports.Map = Map;
