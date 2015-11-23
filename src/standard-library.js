57d0654d385c3008ef47e1cf881765162d110755
Grehhov <grehhov@mail.ru>
2015-11-24 02:21:49 +0500

    Update standard-library.js

--- src/standard-library.js
+++ src/standard-library.js
@@ -1,20 +1,149 @@
 'use strict';
 
 var Collection = function () {
-
+    Collection.prototype.empty.call(this);	
 };
+
+Collection.prototype.pickFirst = function () {
+    var first = this.first;
+    if (this.length > 1) {
+        this.first = this.collection[1];
+        this.length--;
+        this.collection.splice(0, 1);
+    } else {
+        Collection.prototype.empty.call(this);
+    }
+    return first;
+};
+
+Collection.prototype.pickLast = function () {
+    var last = this.last;
+    if (this.length > 1) {
+        this.last = this.collection[this.length - 2];
+        this.length--;
+        this.collection.splice(-1, 1);
+    } else {
+        Collection.prototype.empty.call(this);
+    }
+    return last;
+};
+
+Collection.prototype.insertFirst = function (item) {
+    this.first = item;
+    if (this.isEmpty) {
+        this.last = item;
+    }
+    this.length++;
+    this.isEmpty = false;
+    this.collection = [item].concat(this.collection);	
+}
+
+Collection.prototype.insertLast = function (item) {
+    this.last = item;
+    if (this.isEmpty) {
+        this.first = item;
+    }
+    this.length++;
+    this.isEmpty = false;
+    this.collection = this.collection.concat([item]);	
+}
+
+Collection.prototype.empty = function () {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+    this.isEmpty = true;
+    this.collection = [];
+}
 
 var Queue = function () {
-
+    Queue.prototype.empty.call(this);
 };
+
+Queue.prototype.enqueue = function (item) {
+    this.queue.push(item);
+    this.length++;
+}
+
+Queue.prototype.dequeue = function () {
+    if (this.length !== 0) {
+        var item = this.queue[0];
+        this.queue.splice(0, 1);
+        return item;
+    }
+}
+
+Queue.prototype.empty = function () {
+    this.length = 0;
+    this.queue = [];
+}
 
 var FixedArray = function (size) {
-
+    this.length = size;
+    this.fixedArray = [];
 };
+
+FixedArray.prototype.insertAt = function (index, item) {
+    if (index > this.length - 1) {
+        throw new RangeError;
+    }
+    this.fixedArray[index] = item;
+}
+
+FixedArray.prototype.getAt = function (index) {
+    if (index > this.length - 1) {
+        throw new RangeError;
+    }
+    return this.fixedArray[index];
+}
 
 var Set = function () {
-
+    Set.prototype.empty.call(this);
 };
+
+Set.prototype.insert = function (item) {
+    if (!this.set.hasOwnProperty(item)) {
+        this.set[item] = true;
+        this.length++;
+    }
+}
+
+Set.prototype.remove = function (item) {
+    if (this.set.hasOwnProperty(item)) {
+        delete this.set[item];
+        this.length--;
+    }
+}
+
+Set.prototype.has = function (item) {
+    return this.set.hasOwnProperty(item) ? true : false;
+}
+
+Set.prototype.intersect = function (set) {
+    var result = new Set();
+    Object.keys(this.set).forEach(function (item) {
+        if (set.set.hasOwnProperty(item)) {
+            result.insert(item);
+        }
+    });
+    return result;
+}
+
+Set.prototype.union = function (set) {
+    var result = new Set();
+    Object.keys(this.set).forEach(function (item) {
+        result.insert(item);
+    });
+    Object.keys(set.set).forEach(function (item) {
+        result.insert(item);
+    });
+    return result;
+}
+
+Set.prototype.empty = function () {
+    this.length = 0;
+    this.set = {};
+}
 
 var PriorityQueue = function () {
 
