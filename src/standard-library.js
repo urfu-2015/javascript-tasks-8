3a3f6c528c638c57f7036384209a6aff80554828
KladovValery <valerijkladov@yandex.ru>
2015-11-24 00:25:01 +0500

    Update standard-library.js

--- src/standard-library.js
+++ src/standard-library.js
@@ -1,32 +1,57 @@
 'use strict';
 
 var Collection = function () {
+    this.length = 0;
+    this.isEmpty = true;
+    this.list = [];
+};
+Object.defineProperty(Collection, 'first', { enumerable: true, get: function () {
+    return Collection.list[0];
+}});
+Object.defineProperty(Collection, 'last', { enumerable: true, get: function () {
+    return Collection.list[Collection.list.length - 1];
+}});
 
+Collection.prototype.pickFirst = function () {
+    var first = this.list.shift() || null;
+    this.length = first === null ? 0 : this.length - 1;
+    this.isEmpty = this.length === 0;
+    return first;
 };
 
-var Queue = function () {
-
+Collection.prototype.pickLast = function () {
+    var last = this.list.pop() || null;
+    this.length = last === null ? 0 : this.length - 1;
+    this.isEmpty = this.length === 0;
+    return last;
 };
 
-var FixedArray = function (size) {
-
+Collection.prototype.insertFirst = function (elem) {
+    this.length = this.list.unshift(elem);
+    this.first = elem;
+    this.isEmpty = false;
+    this.last = this.list[this.length - 1];
 };
 
-var Set = function () {
-
+Collection.prototype.insertLast = function (elem) {
+    if (this.isEmpty) {
+        this.isEmpty = false;
+        this.first = elem;
+    }
+    this.list.push(elem);
+    this.length++;
+    this.last = elem;
 };
 
-var PriorityQueue = function () {
-
-};
-
-var Map = function () {
-
+Collection.prototype.empty = function () {
+    this.length = 0;
+    this.isEmpty = true;
+    this.list = [];
 };
 
 exports.Collection = Collection;
-exports.Queue = Queue;
-exports.FixedArray = FixedArray;
-exports.Set = Set;
-exports.PriorityQueue = PriorityQueue;
-exports.Map = Map;
+// exports.Queue = Queue;
+// exports.FixedArray = FixedArray;
+// exports.Set = Set;
+// exports.PriorityQueue = PriorityQueue;
+// exports.Map = Map;
