943a23c80229224a4bd186a365dba5cf9354184e
Даниил <Tkach@air-daniil.at.urfu.ru>
2015-11-24 17:05:59 +0500

    fixed

--- src/standard-library.js
+++ src/standard-library.js
@@ -1,43 +1,40 @@
 'use strict';
 
 var Collection = function () {
-    this.isEmpty = true;
     this.list = [];
 };
-Object.defineProperty(Collection, 'first', { enumerable: true, get: function () {
+Object.defineProperty(Collection.prototype, 'first', { enumerable: true, get: function () {
     return this.list[0];
 }});
-Object.defineProperty(Collection, 'last', { enumerable: true, get: function () {
+Object.defineProperty(Collection.prototype, 'last', { enumerable: true, get: function () {
     return this.list[this.list.length - 1];
 }});
-Object.defineProperty(Collection, 'length', { enumerable: true, get: function () {
+Object.defineProperty(Collection.prototype, 'length', { enumerable: true, get: function () {
     return this.list.length;
+}});
+Object.defineProperty(Collection.prototype, 'isEmpty', { enumerable: true, get: function () {
+    return this.length === 0;
 }});
 
 Collection.prototype.pickFirst = function () {
     var first = this.list.shift() || null;
-    this.isEmpty = this.length === 0;
     return first;
 };
 
 Collection.prototype.pickLast = function () {
     var last = this.list.pop() || null;
-    this.isEmpty = this.length === 0;
     return last;
 };
 
 Collection.prototype.insertFirst = function (elem) {
-    this.isEmpty = false;
     this.list.unshift(elem);
 };
 
 Collection.prototype.insertLast = function (elem) {
-    this.isEpmty = false;
     this.list.push(elem);
 };
 
 Collection.prototype.empty = function () {
-    this.isEmpty = true;
     this.list = [];
 };
 
@@ -45,10 +42,10 @@ var Queue = function () {
     this.queue = [];
 };
 
-Object.defineProperty(Queue, 'length', { enumerable: true, get: function () {
+Object.defineProperty(Queue.prototype, 'length', { enumerable: true, get: function () {
     return this.queue.length;
 }});
-Object.defineProperty(Queue, 'isEpmty', { enumerable: true, get: function () {
+Object.defineProperty(Queue.prototype, 'isEpmty', { enumerable: true, get: function () {
     return this.queue.length === 0;
 }});
 
@@ -96,17 +93,15 @@ FixedArray.prototype.getAt = function (index) {
 };
 
 var Set = function () {
-    this.length = 0;
     this.elements = [];
 };
-// Object.defineProperty(Set, 'length', { enumerable: true, get: function () {
-//     return Set.elements.length;
-// }});
+Object.defineProperty(Set.prototype, 'length', { enumerable: true, get: function () {
+    return this.elements.length;
+}});
 
 Set.prototype.insert = function (item) {
     if (this.elements.indexOf(item) < 0) {
         this.elements.push(item);
-        this.length++;
     }
 };
 
@@ -114,7 +109,6 @@ Set.prototype.remove = function (item) {
     var index = this.elements.indexOf(item);
     if (index > -1) {
         this.elements.splice(index, 1);
-        this.length--;
     }
 };
 
@@ -127,7 +121,6 @@ Set.prototype.intersect = function (set) {
     intersection.elements = this.elements.filter(function (elem) {
         return set.has(elem);
     });
-    intersection.length = intersection.elements.length;
     return intersection;
 };
 
@@ -138,7 +131,6 @@ Set.prototype.union = function (set) {
         return !(set.has(elem));
     });
     union.elements = union.elements.concat(elemFirstSet);
-    union.length = union.elements.length;
     return union;
 };
 
