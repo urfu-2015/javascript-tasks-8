f792862f82e53cb2767f8a1061998ecf6243bfe7
bazhenova <anyusha2595@gmail.com>
2015-11-24 17:11:37 +0500

    Исправления

--- src/standard-library.js
+++ src/standard-library.js
@@ -1,67 +1,71 @@
 'use strict';
 
 var Collection = function () {
-    this.collection = [];
-    this.first = null;
-    this.last = null;
-    this.length = 0;
-    this.isEmpty = true;
+    this.items = [];
 };
 
-Collection.prototype.pickFirst = function () {
-    if (!this.isEmpty) {
-        var element = this.collection.shift();
-        this.length = this.collection.length;
-        this.isEmpty = this.length === 0;
-        if (!this.isEmpty) {
-            this.first = this.collection[0];
-            this.last = this.collection[this.length - 1];
-        }
-        return element;
+Object.defineProperty(Collection.prototype, 'isEmpty', {
+    get: function () {
+        return this.items.length === 0;
     }
-    throw new Error('Коллекция пуста');
+});
+
+Object.defineProperty(Collection.prototype, 'first', {
+    get: function () {
+        if (this.isEmpty) {
+            throw new Error('Коллекция пуста');
+        }
+        return this.items[0];
+    }
+});
+
+Object.defineProperty(Collection.prototype, 'last', {
+    get: function () {
+        if (this.isEmpty) {
+            throw new Error('Коллекция пуста');
+        }
+        return this.items[this.length - 1];
+    }
+});
+
+Object.defineProperty(Collection.prototype, 'length', {
+    get: function () {
+        return this.items.length;
+    }
+});
+
+Collection.prototype.pickFirst = function () {
+    return getElement.bind(this)('first');
 };
 
 Collection.prototype.pickLast = function () {
-    if (!this.isEmpty) {
-        var element = this.collection.pop();
-        this.length = this.collection.length;
-        this.isEmpty = this.length === 0;
-        if (!this.isEmpty) {
-            this.first = this.collection[0];
-            this.last = this.collection[this.length - 1];
-        }
-        return element;
-    }
-    throw new Error('Коллекция пуста');
+    return getElement.bind(this)('last');
 };
 
+function getElement(place) {
+    if (this.isEmpty) {
+        throw new Error('Коллекция пуста');
+    }
+    if (place === 'first') {
+        return this.items.shift();
+    }
+    return this.items.pop();
+}
+
 Collection.prototype.insertFirst = function (element) {
-    this.collection.unshift(element);
-    this.first = element;
-    this.length = this.collection.length;
-    this.last = this.collection[this.length - 1];
-    this.isEmpty = false;
+    this.items.unshift(element);
 };
 
 Collection.prototype.insertLast = function (element) {
-    this.collection.push(element);
-    this.last = element;
-    this.first = this.collection[0];
-    this.isEmpty = false;
-    this.length = this.collection.length;
+    this.items.push(element);
 };
 
 Collection.prototype.empty = function () {
-    this.collection = [];
-    this.isEmpty = true;
-    this.first = null;
-    this.last = null;
+    this.items = [];
 };
 
 var Queue = function () {
-    this.collection = [];
-    this.length = 0;
+    this.items = [];
 };
 
 Queue.prototype = Object.create(Collection.prototype);
@@ -84,22 +88,23 @@ var FixedArray = function (size) {
 };
 
 FixedArray.prototype.insertAt = function (index, item) {
-    if (index > this.length - 1 || index < 0) {
-        throw new RangeError('Некорректный индекс');
-    }
+    check.bind(this)(index);
     this.fixedArray[index] = item;
 };
 
 FixedArray.prototype.getAt = function (index) {
-    if (index > this.length - 1 || index < 0) {
-        throw new RangeError('Некорректный индекс');
-    }
+    check.bind(this)(index);
     return this.fixedArray[index];
 };
 
+function check(index) {
+    if (index > this.length - 1 || index < 0) {
+        throw new RangeError('Некорректный индекс');
+    }
+}
+
 var Set = function () {
-    this.length = 0;
-    this.collection = [];
+    this.items = [];
 };
 
 Set.prototype = Object.create(Collection.prototype);
@@ -111,104 +116,109 @@ Set.prototype.insert = function (item) {
 };
 
 Set.prototype.remove = function (item) {
-    var index = this.collection.indexOf(item);
-    if (index !== -1) {
-        this.collection.splice(index, 1);
-        this.length -= 1;
-    } else {
+    var index = this.items.indexOf(item);
+    if (index === -1) {
         throw new Error('Нет такого элемента');
     }
+    this.items.splice(index, 1);
 };
 
 Set.prototype.has = function (item) {
-    return this.collection.indexOf(item) !== -1;
+    return this.items.indexOf(item) !== -1;
 };
 
 Set.prototype.intersect = function (set) {
-    var intersectSet = new Set();
-    for (var i = 0; i < this.collection.length; i++) {
-        if (set.has(this.collection[i])) {
-            intersectSet.insert(this.collection[i]);
+    var intersectSet = new exports.Set();
+    for (var i = 0; i < this.items.length; i++) {
+        if (set.has(this.items[i])) {
+            intersectSet.insert(this.items[i]);
         }
     }
-    for (var i = 0; i < set.collection.length; i++) {
-        if (intersectSet.has(set.collection[i]) && this.has(set.collection[i])) {
-            intersectSet.insert(set.collection[i]);
+    for (var i = 0; i < set.items.length; i++) {
+        if (intersectSet.has(set.items[i]) && this.has(set.items[i])) {
+            intersectSet.insert(set.items[i]);
         }
     }
     return intersectSet;
 };
 
 Set.prototype.union = function (set) {
-    var unionSet = new Set();
-    for (var i = 0; i < this.collection.length; i++) {
-        unionSet.insert(this.collection[i]);
+    var unionSet = new exports.Set();
+    for (var i = 0; i < this.items.length; i++) {
+        unionSet.insert(this.items[i]);
     }
-    for (var i = 0; i < set.collection.length; i++) {
-        unionSet.insert(set.collection[i]);
+    for (var i = 0; i < set.items.length; i++) {
+        unionSet.insert(set.items[i]);
     }
     return unionSet;
-};
-
-Set.prototype.empty = function () {
-    this.empty();
 };
 
 var PriorityQueue = function () {
     this.queue = new Queue();
     this.priorities = [];
-    this.length = 0;
 };
 
 PriorityQueue.prototype = Object.create(Collection.prototype);
 
 PriorityQueue.prototype.enqueue = function (item, priority) {
     this.queue.enqueue(item);
-    this.length = this.queue.length;
     this.priorities.unshift(priority);
 };
 
 PriorityQueue.prototype.dequeue = function () {
-    if (!this.isEmpty) {
-        var max = Math.max.apply(null, this.priorities);
-        var index = this.priorities.lastIndexOf(max);
-        this.priorities.splice(index, 1);
-        this.length -= 1;
-        return this.queue.collection.splice(index, 1)[0];
+    if (this.isEmpty) {
+        throw new Error('Очередь пуста');
     }
-    throw new Error('Очередь пуста');
+    var max = Math.max.apply(null, this.priorities);
+    var index = this.priorities.lastIndexOf(max);
+    this.priorities.splice(index, 1);
+    return this.queue.items.splice(index, 1)[0];
 };
 
 var Map = function () {
-    this.length = 0;
-    this.map = {};
+    this.map = [];
+    this.items = [];
 };
 
+Object.defineProperty(Map.prototype, 'length', {
+    get: function () {
+        return this.map.length;
+    }
+});
+
+
 Map.prototype.addItem = function (key, item) {
-    this.map[key.toString()] = item;
-    this.length += 1;
+    var index = this.map.indexOf(key);
+    if (index === -1) {
+        this.map.push(key);
+        this.items.push(item);
+    } else {
+        this.items[index] = item;
+    }
 };
 
 Map.prototype.removeItem = function (key) {
-    if (Object.keys(this.map).indexOf(key.toString()) !== -1) {
-        var element = this.map[key.toString()];
-        delete this.map[key.toString()];
-        this.length -= 1;
-        return element;
-    }
-    throw new Error('Нет такого ключа');
+    var index = getIndex.bind(this)(key);
+    this.map.splice(index, 1);
+    this.items.splice(index, 1);
 };
 
 Map.prototype.getItem = function (key) {
-    if (Object.keys(this.map).indexOf(key.toString()) !== -1) {
-        return this.map[key];
-    }
-    throw new Error('Нет такого ключа');
+    var index = getIndex.bind(this)(key);
+    return this.items[index];
 };
 
+function getIndex(key) {
+    var index = this.map.indexOf(key);
+    if (index === -1) {
+        throw new Error('Нет такого ключа');
+    }
+    return index;
+}
+
 Map.prototype.empty = function () {
-    this.map = {};
-    this.length = 0;
+    this.map = [];
+    this.items = [];
 };
 
 exports.Collection = Collection;
